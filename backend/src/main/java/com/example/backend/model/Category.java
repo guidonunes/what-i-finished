bddaab5dee6dcff43f063f34af2ae5c04751991e
Guilherme Nunes <guilherme.augd@gmail.com>
2026-03-30 20:42:02 -0300

    fix: refactor class to enum

--- backend/src/main/java/com/example/backend/model/Category.java
+++ backend/src/main/java/com/example/backend/model/Category.java
@@ -6,47 +6,12 @@ import jakarta.persistence.*;
 import java.util.ArrayList;
 import java.util.List;
 
-@Entity
-@Table(name = "categories")
-public class Category {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
 
-    @Column(length = 50, nullable = false)
-    private String name;
-
-    @OneToMany(mappedBy = "category", cascade = CascadeType.ALL, orphanRemoval = true)
-    private List<Activity> activities = new ArrayList<>();
-
-    public Category() {}
-
-    public Category(String name) {
-        this.name = name;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public List<Activity> getActivities() {
-        return activities;
-    }
-
-    public void setActivities(List<Activity> activities) {
-        this.activities = activities;
-    }
+public enum Category {
+    GAMES,
+    FILMS,
+    BOOKS,
+    COURSES,
+    OTHERS
 }
