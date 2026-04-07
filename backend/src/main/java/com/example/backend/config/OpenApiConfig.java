3cb8867d883d137288d314e280ee785577a6d96e
Guilherme Nunes <guilherme.augd@gmail.com>
2026-04-06 21:02:28 -0300

    fix: add missing import

--- backend/src/main/java/com/example/backend/config/OpenApiConfig.java
+++ backend/src/main/java/com/example/backend/config/OpenApiConfig.java
@@ -1,6 +1,7 @@
 package com.example.backend.config;
 
 import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
